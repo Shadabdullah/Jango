65d8c4a7f785713aeb4d54010cfaf84be2ac2e63
Shadabdullah <shadabdullah101@gmail.com>
2023-05-08 16:01:00 +0530

    App completed

--- app/Empl/urls.py
+++ app/Empl/urls.py
@@ -4,5 +4,8 @@ from .views import *
 
 urlpatterns = [
     path("",emp_home),
-    path('add/',add_emp)
+    path('add/',add_emp),
+    path('delete-emp/<int:emp_id>',deleteEmp),
+    path('update-emp/<int:emp_id>',updateEmp),
+    path('do-update/<int:emp_id>',doUpdate)
 ]
--- app/Empl/views.py
+++ app/Empl/views.py
@@ -7,7 +7,9 @@ from .models import *
 
 # Create your views here.
 def emp_home(request):
-    return render(request ,'emp/home.html',{})
+    emp = Emp.objects.all()
+    
+    return render(request ,'emp/home.html',{"emp" : emp})
 
 def add_emp(request):
     if request.method=="POST":
@@ -37,3 +39,40 @@ def add_emp(request):
   
     
     return render(request, 'emp/add_emp.html',{})
+
+def deleteEmp(request ,emp_id):
+
+    dele = Emp.objects.get(pk=emp_id)
+    dele.delete()
+
+    return redirect("/Empl/")
+
+def updateEmp(request ,emp_id):
+    emp = Emp.objects.get(pk=emp_id)
+    print(emp.department)
+    return render(request, 'emp/update_emp.html',{"emp" : emp})
+
+
+def doUpdate(request,emp_id):
+    if request.method=='POST':
+        emp_name=request.POST.get("emp_name")
+        emp_id_temp=request.POST.get("emp_id")
+        emp_phone=request.POST.get("emp_phone")
+        emp_address=request.POST.get("emp_address")
+        emp_working=request.POST.get("emp_working")
+        emp_department=request.POST.get("emp_department")  
+    
+        e=Emp.objects.get(pk=emp_id)
+        e.name=emp_name
+        e.emp_id=emp_id_temp
+        e.phone=emp_phone
+        e.address=emp_address
+        e.department=emp_department
+        if emp_working is None:
+            e.working=False
+        else:
+            e.working=True
+
+        e.save()
+    return redirect("/Empl/")
+   
